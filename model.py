8ebb653ff99855e6cad2085bb1d54638d2917012
ben-hurd <60407515+ben-hurd@users.noreply.github.com>
2020-12-11 09:41:32 -0500

    Update model.py

--- model.py
+++ model.py
@@ -25,26 +25,24 @@ class Model(tf.keras.Model):
 		self.batch_size = 100
 		self.optimizer = tf.keras.optimizers.Adam()
 		self.loss_list_train = []
-		self.loss_list_test = []
+		self.acc_list = []
 
-		# TODO: experiment with replacing "12"s with "3"s and seeing if it helps. Also could try 'same' padding
 		self.conv_1 = tf.keras.layers.Conv2D(32, [3,12], strides=1, padding='valid', activation='relu')
-
 		self.conv_2 = tf.keras.layers.Conv2D(16, [3,12], strides=1, padding='valid', activation='relu')
+
 		self.conv_2_maxpool = tf.keras.layers.MaxPooling2D(pool_size=[1,12])
 
 		self.conv_3 = tf.keras.layers.Conv2D(64, [3,12], strides=1, padding='valid', activation='relu')
-
 		self.conv_4 = tf.keras.layers.Conv2D(32, [3,12], strides=1, padding='valid', activation='relu')
+
 		self.conv_4_maxpool = tf.keras.layers.MaxPooling2D(pool_size=[1,12])
+
 		self.dropout_1 = tf.keras.layers.Dropout(rate=0.5)
 		self.flatten_1 = tf.keras.layers.Flatten()
-
 		self.dense_1 = tf.keras.layers.Dense(2048, activation='relu', dtype=tf.float32)
+
 		self.dropout_2 = tf.keras.layers.Dropout(rate=0.5)
-
 		self.dense_2 = tf.keras.layers.Dense(512, activation='relu', dtype=tf.float32)
-
 		self.dense_3_out = tf.keras.layers.Dense(18441, activation='sigmoid', dtype=tf.float32)
 
 
@@ -91,6 +89,9 @@ class Model(tf.keras.Model):
 
 		return loss
 
+	def accuracy(self, predictions, actual):
+		correct_predictions = tf.equal(tf.argmax(predictions, 1), tf.argmax(actual, 1))
+		return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
 ###
 # Training & Testing
@@ -126,7 +127,6 @@ def train(model, mix, vocal, instrumental):
 
 		# ideal binary mask for vocal-dominated pixels
 		IBM = tf.squeeze(tf.cast(tf.greater(vocal_batch, instrumental_batch), tf.float32))
-		# print("IBM IN TRAIN:", IBM)
 
 		with tf.GradientTape() as tape:
 			pred = model(mix_batch)
@@ -150,37 +150,37 @@ def test(model, mix, vocal, instrumental):
 	
 	num_examples = len(mix)
 
-	# # shuffle inputs
-	# rand_ind = tf.random.shuffle(range(num_examples))
-	# mix = tf.gather(mix, rand_ind)
-	# vocal = tf.gather(vocal, rand_ind)
-	# instrumental = tf.gather(instrumental, rand_ind)
+	batch_size = 100
 
 	# remove excess to be divisible by batch size
-	batch_remainder = num_examples % model.batch_size
+	batch_remainder = num_examples % batch_size
 	if batch_remainder != 0:
 		mix = mix[:-batch_remainder]
 		vocal = vocal[:-batch_remainder]
 		instrumental = instrumental[:-batch_remainder]
 
+	acc_list = []
 	# batch processing
-	for i in range(0, num_examples, model.batch_size):
-		mix_batch = mix[i : i + model.batch_size]
-		vocal_batch = vocal[i : i + model.batch_size]
-		instrumental_batch = instrumental[i : i + model.batch_size]
+	x = 0
+	for i in range(0, num_examples - batch_remainder, batch_size):
+		x += 1
+		mix_batch = mix[i : i + batch_size]
+		vocal_batch = vocal[i : i + batch_size]
+		instrumental_batch = instrumental[i : i + batch_size]
 
 		# ideal binary mask for vocal-dominated pixels
 		IBM = tf.squeeze(tf.cast(tf.greater(vocal_batch, instrumental_batch), tf.float32))
 
 		pred = model(mix_batch)
-		if i == 0:
-			print(pred)
-			print(pred.shape)
-		loss = model.loss(pred, IBM)
+		pred = tf.cast(tf.greater(pred, 0.3), tf.float32)
 
-		model.loss_list_test.append(loss)
+		correct_predictions = tf.cast(tf.equal(pred,IBM), tf.float32)
 
-	return
+		acc = tf.reduce_sum(correct_predictions).numpy() / tf.size(pred).numpy()
+
+		acc_list.append(acc)
+
+	return sum(acc_list[:-1]) / x
 
 
 # (copied from hw2)
@@ -207,96 +207,90 @@ def main():
 	:return: None
 	"""
 	
+	## run file with -train flag to train the model for 5 epochs and print its accuracy on the training set 
+	## model will be saved at the end to "trained_model"
 	if sys.argv[1] == "-train":
 
 		train_mix, train_vocals, train_instrumental = get_data("data/spectrograms/train")
 		test_mix, test_vocals, test_instrumental = get_data("data/spectrograms/test")
+
 		model = Model()
+
 		for i in range(5):
 
 			train(model, train_mix, train_vocals, train_instrumental)
 			print("End of Epoch:", i+1)
 
-			# test(model, test_mix, test_vocals, test_instrumental)
-
-		print("Train loss per batch:")
+		print("Training loss:")
 		visualize_loss(model.loss_list_train)
 
-		# print("Test loss per batch")
-		# visualize_loss(model.loss_list_test)
-
+		acc = test(model, test_mix, test_vocals, test_instrumental)
+		print("Model Accuracy is:", acc)
 		model.save("trained_model")
 		return
-	
+
+	## run file with -savedWeights flag to used the saved model and print its accuracy on the training set
+	## 2nd flag that references a .wav mix can be added and an isolated-vocals.wav file will be produced
+	## that is our model's attempt at isolating the vocals from that song (likely won't completely finish due to errors in our reshaping)
 	elif sys.argv[1] == "-savedWeights":
+
+		# load model
 		model = keras.models.load_model('trained_model', compile=False)
-		# model.built = True
-		# load_status = model.load_weights("trained")
-		# load_status.assert_consumed()
-		mus_data = musdb.DB(root="data/musdb18", subsets="test")
-		# train_mix, train_vocals, train_instrumental = get_data("data/spectrograms/train")
 
-		original_sr = mus_data[0].rate
-		target_sr = 22050
-		mix_data = librosa.resample(librosa.to_mono(mus_data[0].audio.T), orig_sr=original_sr, target_sr=target_sr, res_type='kaiser_best', fix=True, scale=False)
-		
-		len_frame = target_sr*3
-		num_frames = int(len(mix_data)/len_frame)
+		# test saved model
+		test_mix, test_vocals, test_instrumental = get_data("data/spectrograms/test")
 
-		totalMels = []
+		# print accuracy 
+		acc = test(model, test_mix, test_vocals, test_instrumental)
+		print("Model Accuracy is:", acc)
 
-		for frame in range(num_frames):
-			n_fft = 4096
-			hop_length = 256
-			n_mels = 128
-			sample_rate = 22050
-			w_length = 1024
+		# if 2nd flag exists, treat it as a .wav file of a full mix, and save model's output to isolated_vocals.wav
+		if sys.argv[2]:
+			y, sr = librosa.load(sys.argv[2], sr=None)
+			original_sr = sr
+			target_sr = 22050
+			mix_data = librosa.resample(librosa.to_mono(y), orig_sr=original_sr, target_sr=target_sr, res_type='kaiser_best', fix=True, scale=False)
+			
+			len_frame = target_sr*3
+			num_frames = int(len(mix_data)/len_frame)
 
-			# Librosa melspectrogram
-			mels = librosa.feature.melspectrogram(mix_data[frame * len_frame : frame * len_frame + len_frame], sr=sample_rate, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels, win_length=w_length)
-			totalMels.append(mels)
-		x = len(totalMels)
-		y = len(totalMels[0])
-		z = len(totalMels[0][0])
+			totalMels = []
 
-		Input = slice_spectrogram(totalMels)
-		# print(Input)
-		IBM = model(Input)
-		IBM = tf.squeeze(tf.cast(tf.greater(IBM, 0.3), tf.float32))
-		print(tf.reduce_sum(IBM))
-		vocals = tf.multiply(IBM,tf.cast(tf.squeeze(Input),tf.float32))
-		vocals = tf.squeeze(tf.reshape(vocals,[-1,1]))
+			for frame in range(num_frames):
+				n_fft = 4096
+				hop_length = 256
+				n_mels = 128
+				sample_rate = 22050
+				w_length = 1024
 
-		print("VOCAL SHAPE:", vocals.shape)
-		difference = x * y * z - vocals.shape[0]
-		vocals = tf.cast(vocals.numpy(), tf.float32)
-		pad = tf.cast(tf.zeros(difference), tf.float32)
-		print("PAD", pad)
-		print(vocals)
-		vocals = tf.concat([vocals,pad],0)
-		print(vocals.shape)
-		vocals = tf.reshape(vocals, [y,x*z])
-		print("vocals are", vocals)
-		print(vocals.numpy()[:,:259].shape)
-		spectrogram_to_audio(vocals.numpy()[:,:259], "vocals-try-again???")
+				# Librosa melspectrogram
+				mels = librosa.feature.melspectrogram(mix_data[frame * len_frame : frame * len_frame + len_frame], sr=sample_rate, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels, win_length=w_length)
+				totalMels.append(mels)
+
+			x = len(totalMels)
+			y = len(totalMels[0])
+			z = len(totalMels[0][0])
+
+			Input = slice_spectrogram(totalMels)
+
+			IBM = model(Input)
+			IBM = tf.squeeze(tf.cast(tf.greater(IBM, 0.0), tf.float32))
+
+			vocals = tf.multiply(IBM,tf.cast(tf.squeeze(Input),tf.float32))
+			vocals = tf.squeeze(tf.reshape(vocals,[-1,1]))
+
+
+			difference = x * y * z - vocals.shape[0]
+			vocals = tf.cast(vocals.numpy(), tf.float32)
+			pad = tf.cast(tf.zeros(difference), tf.float32)
+
+			vocals = tf.concat([vocals,pad],0)
+
+			vocals = tf.reshape(vocals, [y,x*z])
+			# likely will freeze here due to the way we reshape :(
+			spectrogram_to_audio(vocals.numpy(), "isolated-vocals.wav")
+
 		return 
-
-	# train_mix, train_vocals, train_instrumental = get_data("data/spectrograms/train")
-	# test_mix, test_vocals, test_instrumental = get_data("data/spectrograms/test")
-
-	# # TRIAL VALUES:
-	# train_mix = tf.constant(tf.random.truncated_normal([5000,9,2049,1],stddev=0.1), dtype=tf.float32)
-	# train_vocals = tf.constant(tf.random.truncated_normal([5000,9,2049,1],stddev=0.1))
-	# train_instrumental = tf.constant(tf.random.truncated_normal([5000,9,2049,1],stddev=0.1),dtype=tf.float32)
-
-	# test_mix = tf.constant(tf.random.truncated_normal([5000,9,2049,1],stddev=0.1), dtype=tf.float32)
-	# test_vocals = tf.constant(tf.random.truncated_normal([5000,9,2049,1],stddev=0.1))
-	# test_instrumental = tf.constant(tf.random.truncated_normal([5000,9,2049,1],stddev=0.1),dtype=tf.float32)
-
-	# # example of how to go from spectrogram -> audio
-	# spectrogram_to_audio(train_mix[5000],"data/train-mix-1.wav")
-	# spectrogram_to_audio(train_vocals[5000],"data/train-vocals-1.wav")
-	# spectrogram_to_audio(train_instrumental[5000],"data/train-instrumental-1.wav")
 
 	return
 
